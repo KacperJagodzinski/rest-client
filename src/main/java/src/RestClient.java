c8237c89465dd54dfe06dacd3ca1310223c0e7eb
KacperJagodzinski <kacper.jagodzinski@onet.eu>
2020-11-08 18:22:38 +0100

    2nd way, JsonObject

--- src/main/java/src/RestClient.java
+++ src/main/java/src/RestClient.java
@@ -1,6 +1,8 @@
 package src;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,11 +13,17 @@ public class RestClient {
 
     public static void main(String[] args) throws IOException {
 
-        URL url = new URL("https://cat-fact.herokuapp.com/facts/random");
-        InputStreamReader reader = new InputStreamReader(url.openStream());
+        URL CatFactUrl = new URL("https://cat-fact.herokuapp.com/facts/random");
+        InputStreamReader CatFactReader = new InputStreamReader(CatFactUrl.openStream());
 
-        CatFact catFact = new Gson().fromJson(reader,CatFact.class);
-        System.out.println(catFact.toString());
+        CatFact catFact = new Gson().fromJson(CatFactReader,CatFact.class);
+        System.out.println(catFact.getText());
+
+        //tymczasowe zapisywanie (jednolinijokwa odpowiedz)
+        URL PicUrl = new URL("https://aws.random.cat/meow");
+        InputStreamReader PicReader = new InputStreamReader(PicUrl.openStream());
+        JsonObject jsonObject = new JsonParser().parse(PicReader).getAsJsonObject();
+        System.out.println(jsonObject.get("file").getAsString());
 
     }
 }
